ca7fc99c6e35bd4ce86e33195dfdadb1aa40af10
bokangleqele <bokangleqele7@gmail.com>
2024-09-10 09:07:09 +0100

    added button that's supposed to view all courses

--- app/index.tsx
+++ app/index.tsx
@@ -2,97 +2,122 @@ import React from 'react';
 import { Stack, Link } from 'expo-router';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
 import { Heading } from '@gluestack-ui/themed';
-import { Text, Image, ScrollView, View } from 'react-native'; 
+import { Text, Image, ScrollView, View } from 'react-native';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      
+
       {/* Wrapping everything inside a vertical ScrollView */}
       <ScrollView showsVerticalScrollIndicator={true}>
         <Heading style={{ fontSize: 32, fontWeight: '500', marginHorizontal: 5 }}>Courses</Heading>
-        <Text style={{ marginHorizontal: 5, fontSize: 16, marginBottom: -20  }}>What do you wanna learn?</Text>
-        
-        {/* First Container */}
-        <Container>
-          <Heading style={{marginHorizontal: -30, fontSize: 20, fontWeight: '400' }}>Programming</Heading>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <View style={{ flexDirection: 'row', paddingVertical: 10 }}>
-              <Image
-                source={require('../assets/favicon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 1 }}
-                alt="Programming Image" 
-              />
-              <Image
-                source={require('../assets/favicon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 80 }}
-                alt="Programming Image" 
-              />
-              <Image
-                source={require('../assets/icon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 20 }}
-                alt="Programming Image" 
-              />
+        <Text style={{ marginHorizontal: 5, fontSize: 16, marginBottom: -20 }}>What do you wanna learn?</Text>
+
+        {/* First Container - Programming */}
+        <View style={{ marginBottom: -50 }}>
+          <Container>
+            {/* Row layout for Heading and See More Button */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Heading style={{ marginHorizontal: -30, fontSize: 20, fontWeight: '400' }}>Programming</Heading>
+              <Link href={{ pathname: '/details', params: { category: 'Programming' } }} asChild>
+                <Button title="View all" />
+              </Link>
             </View>
-          </ScrollView>
-        </Container>
 
-        {/* Second Container */}
-        <Container>
-          <Heading style={{marginHorizontal: -30, fontSize: 20, fontWeight: '400' }}>Software Engineering</Heading>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <View style={{ flexDirection: 'row', paddingVertical: 10 }}>
-              <Image
-                source={require('../assets/favicon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 1 }}
-                alt="Programming Image" 
-              />
-              <Image
-                source={require('../assets/favicon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 80 }}
-                alt="Programming Image" 
-              />
-              <Image
-                source={require('../assets/icon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 20 }}
-                alt="Programming Image" 
-              />
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={{ flexDirection: 'row', paddingVertical: 10 }}>
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 1 }}
+                  alt="Programming Image"
+                />
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 80 }}
+                  alt="Programming Image"
+                />
+                <Image
+                  source={require('../assets/icon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 20 }}
+                  alt="Programming Image"
+                />
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 1 }}
+                  alt="Programming Image"
+                />
+              </View>
+            </ScrollView>
+          </Container>
+        </View>
+
+        {/* Second Container - Software Engineering */}
+        <View style={{ marginBottom: -50 }}>
+          <Container>
+            {/* Row layout for Heading and See More Button */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Heading style={{ marginHorizontal: -30, fontSize: 20, fontWeight: '400' }}>Software Engineering</Heading>
+              <Link href={{ pathname: '/details', params: { category: 'Software Engineering' } }} asChild>
+                <Button title="View all" />
+              </Link>
             </View>
-          </ScrollView>
 
-        </Container>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={{ flexDirection: 'row', paddingVertical: 10 }}>
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 1 }}
+                  alt="Software Engineering Image"
+                />
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 80 }}
+                  alt="Software Engineering Image"
+                />
+                <Image
+                  source={require('../assets/icon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 20 }}
+                  alt="Software Engineering Image"
+                />
+              </View>
+            </ScrollView>
+          </Container>
+        </View>
 
-{/* third Container */}
-        <Container>
-          <Heading style={{marginHorizontal: -30, fontSize: 20, fontWeight: '400' }}>Artificial Intelligence</Heading>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            <View style={{ flexDirection: 'row', paddingVertical: 10 }}>
-              <Image
-                source={require('../assets/favicon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 1 }}
-                alt="Programming Image" 
-              />
-              <Image
-                source={require('../assets/favicon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 80 }}
-                alt="Programming Image" 
-              />
-              <Image
-                source={require('../assets/icon.png')} 
-                style={{ width: 200, height: 150, marginHorizontal: 20 }}
-                alt="Programming Image" 
-              />
+        {/* Third Container - Artificial Intelligence */}
+        <View style={{ marginBottom: -50 }}>
+          <Container>
+            {/* Row layout for Heading and See More Button */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Heading style={{ marginHorizontal: -30, fontSize: 20, fontWeight: '400' }}>Artificial Intelligence</Heading>
+              <Link href={{ pathname: '/details', params: { category: 'Artificial Intelligence' } }} asChild>
+                <Button title="View all" />
+              </Link>
             </View>
-          </ScrollView>
 
-          <Link href={{ pathname: '/details', params: { name: 'Dan' }}} asChild>
-            <Button title="Show Details" />
-          </Link>
-        </Container>
-
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={{ flexDirection: 'row', paddingVertical: 10 }}>
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 1 }}
+                  alt="Artificial Intelligence Image"
+                />
+                <Image
+                  source={require('../assets/favicon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 80 }}
+                  alt="Artificial Intelligence Image"
+                />
+                <Image
+                  source={require('../assets/icon.png')}
+                  style={{ width: 200, height: 150, marginHorizontal: 20 }}
+                  alt="Artificial Intelligence Image"
+                />
+              </View>
+            </ScrollView>
+          </Container>
+        </View>
       </ScrollView>
     </>
   );
